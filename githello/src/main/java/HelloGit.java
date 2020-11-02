f0fb79888bddbcde5cdeea74cff1ad68a091369b
deng_g <deng_g@qq.com>
2020-11-02 16:20:25 +0800

    第一次提交

---
+++ githello/src/main/java/HelloGit.java
@@ -0,0 +1,11 @@
+/**
+ * @description:
+ * @author: admin
+ * @date: Created in 2020/11/2 16:07
+ * @modified By:
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
